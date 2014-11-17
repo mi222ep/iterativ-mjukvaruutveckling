1eacfabcc597ab826a12c8330cd33bc5db2b2b56
Matila Ingman <mi222ep@student.liu.se>
2014-11-17 13:22:16 +0100

    Uppgift 1 A och B

---
+++ RaknaSiffror/RaknaSiffror/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaknaSiffror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mata in ett heltal:");
+            int N = int.Parse(Console.ReadLine());
+            int zeroCount = 0;
+            int evenCount = 0;
+            int unevenCount = 0;
+
+            string numberString = N.ToString();
+
+            char[] character = numberString.ToCharArray();
+
+            for (int i = 0; i <= (numberString.Length - 1); i++)
+            {
+                if(character[i] == '0') {
+                    
+                    zeroCount++;
+                }
+
+                else if (character[i] % 2 == 0 )
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    unevenCount++;
+                }
+            }
+            Console.WriteLine("Nollor: {0}, jämna: {1}, udda: {2}", zeroCount, evenCount, unevenCount);
+
+        }
+    }
+}
---
+++ uppgift 1A/uppgift 1A/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mata in en textrad så räknar jag antal a och A:");
+            int counta = 0;
+            int countA  = 0;
+            string input = Console.ReadLine();
+
+            char[] character = input.ToCharArray();
+
+            for (int i = 0; i <= (character.Length - 1); i++) {
+                
+                if (character[i] == 'A') {
+                    countA++;
+                }
+                if (character[i] == 'a') {
+                    counta++;
+                }
+            }
+
+            Console.WriteLine("antal a: {0}, antal A: {1}.", counta, countA);
+            
+        }
+    }
+}
